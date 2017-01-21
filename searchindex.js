76c8bbc874eb6dc1ecb68ebf3669d9796ace2c78
Michael Bardsley <mhbardsley@gmail.com>
2017-01-21 23:13:05 +1100

    rebuilt docs

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:50,filenames:["includeme","index"],objects:{},objnames:{},objtypes:{},terms:{content:1,index:1,modul:1,page:1,search:1},titles:["&lt;no title&gt;","Welcome to sphinxdoc-test&#8217;s documentation!"],titleterms:{document:1,indice:1,sphinxdoc:1,tabl:1,test:1,welcom:1}})
